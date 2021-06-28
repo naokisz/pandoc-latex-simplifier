37e54b88db08996a344d88d27b829a64921968d6
naoki suzuki <naokihirokitomoki@gmail.com>
2021-06-28 13:48:53 +0900

    initial commit

---
+++ markdown.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+pandoc --wrap=preserve -f markdown $1 -t latex | sed -e '/^\\hypertarget/d' | sed -e 's/\\label.*$//g' | sed -e '/\\tightlist/d' | sed -e '/^$/d' | sed -z -e 's/\\item\n\s*/\\item /g' | sed -e '/^\\subsection/{x;p;x;G}' | sed -e '/^\\section/{x;p;x;G}' | sed -z -e 's/\n\n\n/\n\n/g' | sed -e 's/\\section/\\paragraph/g' | sed -E 's/\\(sub)+section/\\subparagraph/g' | unexpand -t 2 | latexindent -
