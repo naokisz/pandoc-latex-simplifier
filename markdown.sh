92a5254e066d1c1245020b5c324d98919989bbbb
naoki suzuki <naokihirokitomoki@gmail.com>
2021-07-28 21:20:08 +0900

    fix enumerate and math equation and looks

--- markdown.sh
+++ markdown.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-pandoc --wrap=preserve -f markdown $1 -t latex | sed -e '/^\\hypertarget/d' | sed -e 's/\\label.*$//g' | sed -e '/\\tightlist/d' | sed -e '/^$/d' | sed -z -e 's/\\item\n\s*/\\item /g' | sed -e '/^\\subsection/{x;p;x;G}' | sed -e '/^\\section/{x;p;x;G}' | sed -z -e 's/\n\n\n/\n\n/g' | sed -e 's/\\section/\\paragraph/g' | sed -E 's/\\(sub)+section/\\subparagraph/g' | unexpand -t 2 | latexindent -
+pandoc --wrap=preserve -f markdown $1 -t latex \
+	| sed -e '/^\\hypertarget/d' \
+	| sed -e 's/\\label.*$//g' \
+	| sed -e '/\\tightlist/d' \
+	| sed -e '/^$/d' \
+	| sed -z -e 's/\\item\n\s*/\\item /g' \
+	| sed -e '/^\\subsection/{x;p;x;G}' \
+	| sed -e '/^\\section/{x;p;x;G}' \
+	| sed -z -e 's/\n\n\n/\n\n/g' \
+	| sed -e 's/\\section/\\paragraph/g' \
+	| sed -E 's/\\(sub)+section/\\subparagraph/g' \
+	| sed -e "s/\\\(/\$/g" \
+	| sed -e "s/\\\)/\$/g" \
+	| sed -e '/\\def/d' \
+	| unexpand -t 2 \
+	| latexindent -
